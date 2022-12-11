377093cf7d0ab654a7d8c5a2716e813c78f6a93c
Kunjan Patel <76856439+kunjan2021@users.noreply.github.com>
2022-12-11 12:59:53 -0500

    Add files via upload

---
+++ Keyless car.js
@@ -0,0 +1,9 @@
+var age = prompt("How old are you?");
+
+if (number(age) < 18) {
+    alert("Sorry!!!");
+} else if (number(age) > 18){
+   alert("Enjoy!!!") ;
+} else if (number(age) ===18){
+    alert("Enjoy First Ride!!");
+}
